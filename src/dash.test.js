408264fa454f1842997fdf5a32c06462773ef805
Edward Pasenidis <me@edpasenidis.tech>
2020-08-30 23:05:46 +0300

    feat: unit tests

--- src/dash.test.js
+++ src/dash.test.js
@@ -7,3 +7,15 @@ test("give 1 + 1 and expect 2 as a result", () => {
 test("give the area of a triangle", () => {
   expect(dash.triangle.area(3, 4)).toBe(6);
 });
+
+test("give the maximum", () => {
+  expect(dash.statistics.max(5, 10)).toBe(10);
+});
+
+test("give the minimum", () => {
+  expect(dash.statistics.min(5, 10)).toBe(5);
+});
+
+test("give the average", () => {
+  expect(dash.statistics.avg(1, 2)).toBe(1.5);
+});
